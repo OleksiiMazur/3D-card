5e99c635e10867537f9e96e074eb476a3ad0a88e
Oleksii M <oleksiimazur111@gmail.com>
2020-02-11 13:10:18 +0200

    Fix bottom-top position of card on mousemove.

--- card.js
+++ card.js
@@ -4,14 +4,17 @@ if ( location.protocol !== "https:" ) {
     location.href = "https:" + window.location.href.substring( window.location.protocol.length );
 }
 
-function deviceOrientation($card, $body) {
+function deviceOrientation(){
+    let $card = document.querySelector('.card');
+    let $body = document.querySelector('body');
 
     function desktopParallax() {
         document.addEventListener('mousemove', function (e) {
-            let windWidth= window.innerWidth;
-            let windHeight= window.innerHeight;
-            let leftRight = -(windWidth / 2 - e.pageX) / 35;
-            let bottomTop = (windHeight - e.pageY) / 20;
+            let leftRight  = -(window.innerWidth / 2 - e.pageX) / 35;
+            let bottomTop  = (window.innerHeight / 2 - e.pageY + pageYOffset + $card.offsetHeight) / 20;
+
+            console.log('bottomTop: ' + bottomTop);
+            console.log('pageYOffset: ' + pageYOffset);
 
             $card.style.transform = 'rotateX(' + (bottomTop) + 'deg) rotateY(' + (leftRight) + 'deg)';
             // document.querySelector('.card__info').textContent  = 'bottomTop:\ ' + bottomTop + 'deg' + '\n leftRight:\ ' + leftRight + 'deg';
@@ -67,79 +70,85 @@ function deviceOrientation($card, $body) {
         });
     }
 
-    sensorsChecker.checkDeviceorientation(function(){ //check the sensors for the deviceorientation api
-        // alert('Sensor detected');
-
-        function getCookie(name) {
-            let matches = document.cookie.match(new RegExp(
-                "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-            ));
-            return matches ? decodeURIComponent(matches[1]) : undefined;
-        }
-        function setCookie(name, value, options = {}) {
-            options = {
-                path: '/',
-            };
-            let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-
-            for (let optionKey in options) {
-                updatedCookie += "; " + optionKey;
-                let optionValue = options[optionKey];
-                if (optionValue !== true) {
-                    updatedCookie += "=" + optionValue;
+    sensorsChecker.checkDeviceorientation(
+        function(){ // check if the device has sensors
+            if (window.DeviceOrientationEvent) { // and check if the browser supports DeviceOrientationEvent
+                alert('Sensor detected, \nbrowser supports \nDeviceOrientationEvent');
+                function getCookie(name) {
+                    let matches = document.cookie.match(new RegExp(
+                        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+                    ));
+                    return matches ? decodeURIComponent(matches[1]) : undefined;
                 }
-            }
-            document.cookie = updatedCookie;
-        }
-        function deleteCookie(name) {
-            setCookie(name, "", {
-                'max-age': -1
-            })
-        }
+                function setCookie(name, value, options = {}) {
+                    options = {
+                        path: '/',
+                    };
+                    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
 
-        if( !getCookie('device_orientation_request_granted') ){
-            sendRequest();
-        } else if (getCookie('device_orientation_request_granted')) {
-            deviceOrientationParallax();
-        }
-
-        function sendRequest(){ //send request for using DeviceOrientationEvent ==> start deviceOrientationParallax if grated or don`t need
-            if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-                $body.addEventListener('click', function () { // feature detect
-                    DeviceOrientationEvent.requestPermission().then(permissionState => {
-                        if (permissionState === 'granted') {
-                            setCookie('device_orientation_request_granted', 'granted', {
-                                secure: false,
-                                'samesite': 'lax',
-                                'max-age': 1000000,
-                            });
-
-                            deviceOrientationParallax();
+                    for (let optionKey in options) {
+                        updatedCookie += "; " + optionKey;
+                        let optionValue = options[optionKey];
+                        if (optionValue !== true) {
+                            updatedCookie += "=" + optionValue;
                         }
-                    }).catch(console.error);
-                });
+                    }
+                    document.cookie = updatedCookie;
+                }
+                function deleteCookie(name) {
+                    setCookie(name, "", {
+                        'max-age': -1
+                    })
+                }
+
+                function sendRequest(){ //send request for using DeviceOrientationEvent ==> start deviceOrientationParallax if grated or don`t need
+                    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+                        $body.addEventListener('click', function () { // feature detect
+                            DeviceOrientationEvent.requestPermission().then(permissionState => {
+                                if (permissionState === 'granted') {
+                                    setCookie('device_orientation_request_granted', 'granted', {
+                                        secure: false,
+                                        'samesite': 'lax',
+                                        'max-age': 1000000,
+                                    });
+
+                                    deviceOrientationParallax();
+                                }
+                            }).catch(console.error);
+                        });
+                    } else {
+                        deviceOrientationParallax();
+                    }
+                }
+
+                if( !getCookie('device_orientation_request_granted') ){
+                    sendRequest();
+                } else if (getCookie('device_orientation_request_granted')) {
+                    deviceOrientationParallax();
+                }
+            } else if (!window.DeviceOrientationEvent) {
+                alert('Sensor detected, \nbut the browser doesn`t support \nDeviceOrientationEvent');
+                scrollParallax();
             } else {
-                deviceOrientationParallax();
+                scrollParallax();
+            }
+        },
+        function(){
+            if (window.innerWidth <= 1024) {
+                alert('no sensor detected, \nMOBILE or TAB without sensors');
+                scrollParallax();
+            } else {
+                alert('no sensor detected, \nDESKTOP');
+                desktopParallax();
+                // scrollParallax();
             }
         }
-    },function(){
-        if (window.innerWidth <= 1024) {
-            // alert('no sensor detected, \nMOBILE or TAB without sensors');
-            scrollParallax();
-        } else {
-            // alert('no sensor detected, \nDESKTOP');
-            desktopParallax();
-            // scrollParallax();
-        }
-    });
+    );
 };
 
 document.addEventListener("DOMContentLoaded", function(){
-    let $card = document.querySelector('.card');
-    let $body = document.querySelector('body');
-
-    deviceOrientation($card, $body);
+    deviceOrientation();
     window.onresize = function(){
-        deviceOrientation($card, $body);
+        deviceOrientation();
     }
 });
